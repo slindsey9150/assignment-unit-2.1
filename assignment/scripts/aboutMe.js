9a30d1fb10f59bc2dfc84461601126c5b4591c57
Stevie Lindsey Jr <stevie.lindsey.jr@gmail.com>
2025-01-01 11:26:26 -0600

    redoing assignment

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -2,16 +2,19 @@
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 let firstName = 'Stevie';
 console.log(firstName)
+let firstName2 = 'Steebee'
 
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 let lastName = 'Lindsey';
 console.log(lastName);
+let lastName2 = 'Leensee'
 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
 let fullName = firstName + ' ' + lastName ;
 console.log(fullName);
+let fullName2 = firstName2 + ' ' + lastName2
 
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
